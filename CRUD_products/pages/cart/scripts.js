be53e17513549f6d92cf4ff7b4aee65081b4da4d
Khuongktn <khuongkt146@gmail.com>
2024-09-13 20:46:17 +0700

    update CRUD_products

---
+++ CRUD_products/pages/cart/scripts.js
@@ -0,0 +1,60 @@
+const $container_cart = document.getElementById('container-cart');
+const $total_price = document.getElementById('total-price');
+
+const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+const formatter = new Intl.NumberFormat('en-US', {
+	style: 'currency',
+	currency: 'VND',
+});
+
+function renderCart() {
+	let htmlCart = '';
+	let totalPrice = 0;
+
+	for (let item of cart) {
+		totalPrice = totalPrice + item.price * item.quantity;
+		htmlCart += `
+        <tr>
+			<td class="p-4" style="width: 400px">
+				<div class="media align-items-center d-flex">
+					<img
+						src="${item.image}"
+						class="d-block ui-w-40 ui-bordered mr-4"
+						alt=""
+					/>
+					<div class="media-body" style="margin-left: 12px">
+						<a href="#" class="d-block text-dark">${item.title}</a>
+					</div>
+				</div>
+			</td>
+			<td class="text-right font-weight-semibold align-middle p-4">
+				${formatter.format(item.price)}
+			</td>
+			<td class="align-middle p-4">
+				<input
+					type="text"
+					class="form-control text-center"
+					value=${item.quantity}
+				/>
+			</td>
+			<td class="text-right font-weight-semibold align-middle p-4">
+				${formatter.format(item.quantity * item.price)}
+			</td>
+			<td class="text-center align-middle px-0">
+				<a
+					href="#"
+					class="shop-tooltip close float-none text-danger"
+					title=""
+					data-original-title="Remove"
+				>×</a>
+			</td>
+		</tr>
+        `;
+	}
+
+	$container_cart.innerHTML = htmlCart;
+	$total_price.innerHTML = `<strong>${formatter.format(totalPrice)} VND</strong>`;
+}
+
+renderCart();
--- CRUD_products/pages/list-products/scripts.js
+++ CRUD_products/pages/list-products/scripts.js
@@ -4,7 +4,7 @@
 // JSON.parse(data) => chuyển đổi data từ định dạng json về kiểu dữ liệu ban đầu
 
 const listProduct = JSON.parse(localStorage.getItem('listProduct')) || [];
-console.log(listProduct);
+const cart = JSON.parse(localStorage.getItem('cart')) || [];
 // Các input
 const $idProduct = document.getElementById('id');
 const $titleProduct = document.getElementById('title');
@@ -13,6 +13,16 @@ const $imageProduct = document.getElementById('image');
 const $descriptionProduct = document.getElementById('description');
 // Các button
 const $btnCreateProduct = document.getElementById('create-product');
+const $btnUpdateProduct = document.getElementById('update-product');
+// Modal bootstrap
+const myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'), {
+	keyboard: false,
+});
+
+const formatter = new Intl.NumberFormat('en-US', {
+	style: 'currency',
+	currency: 'VND',
+});
 
 // Bước 1: gán event cho button thêm mới
 $btnCreateProduct.addEventListener('click', function () {
@@ -63,9 +73,27 @@ function renderListProduct() {
 						/>
 						<div class="card-body">
 							<h5 class="card-title">${product.title}</h5>
-							<p class="card-text mt-1 mb-1">${product.price} VND</p>
+							<p class="card-text mt-1 mb-1">${formatter.format(product.price)}</p>
 							<p class="card-text">${product.description}</p>
-							<button class="btn btn-primary">Xem chi tiết</button>
+							<div>
+								<button class="btn btn-primary">Xem chi tiết</button>
+								<button onclick="addToCart(${
+									product.id
+								})" type="button" class="btn btn-secondary" style="margin-left: 8px"><i class="fa-solid fa-cart-plus"></i></button>
+								<div class="btn-group">
+								<button class="btn btn-secondary dropdown-toggle" type="button" style="margin-left: 8px" id="defaultDropdown" data-bs-toggle="dropdown" data-bs-auto-close="true" aria-expanded="false">
+									<i class="fa-solid fa-ellipsis-vertical"></i>
+								</button>
+								<ul class="dropdown-menu" aria-labelledby="defaultDropdown">
+									<li><a class="dropdown-item" href="#" onclick="getProductUpdate(${
+										product.id
+									})">Cập nhật</a></li>
+									<li><a class="dropdown-item" href="#" onclick="deleteProduct(${
+										product.id
+									})">Xóa</a></li>
+								</ul>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -76,3 +104,126 @@ function renderListProduct() {
 }
 
 renderListProduct();
+
+// Bước 1: Gán function cho button add to cart
+function addToCart(productId) {
+	let productFind = null;
+	// Bước 2: Khi function được gọi =>
+	// xử lý để lấy được thông tin sản phẩm mà người dùng click
+	for (let product of listProduct) {
+		if (product.id === productId) {
+			productFind = product;
+		}
+	}
+	// Bước 3: Xử lý thông tin sản phẩm
+	let indexProduct = -1;
+	for (let i = 0; i < cart.length; i++) {
+		if (productFind.id === cart[i].id) {
+			indexProduct = i;
+		}
+	}
+	// TH1: Nếu trong giỏ hàng chưa tồn tại sản phẩm
+	// 	=> Thêm mới sản phẩm vào trong giỏ hàng
+	if (indexProduct === -1) {
+		const newProduct = {
+			id: productFind.id,
+			title: productFind.title,
+			image: productFind.image,
+			price: productFind.price,
+			quantity: 1,
+		};
+		cart.push(newProduct);
+	} else {
+		// 	TH2: Nếu trong giỏ hàng đã tồn tại sản phẩm
+		// 	=> Tăng số lượng lên
+		cart[indexProduct].quantity = cart[indexProduct].quantity + 1;
+	}
+	// Bước 4: Sau khi xử lý dữ liệu => lưu thông tin vào trong local storage
+	localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+// Bước 1: Gán function cho button Xóa
+function deleteProduct(productId) {
+	console.log('productId: ', productId);
+	// Bước 2: Khi function được gọi =>
+	// xử lý để lấy được vị trí sản phẩm trong danh sách sản phẩm (index product trong listProduct)
+	let indexProduct = -1;
+	for (let i = 0; i < listProduct.length; i++) {
+		if (listProduct[i].id === productId) {
+			indexProduct = i;
+		}
+	}
+	// Bước 3: Xóa sản phẩm khỏi danh sách (xóa phần tử khỏi mảng)
+	listProduct.splice(indexProduct, 1);
+	// Bước 4: Sau khi xử lý dữ liệu => lưu thông tin vào trong local storage
+	localStorage.setItem('listProduct', JSON.stringify(listProduct));
+	// Bước 5: Gọi lại hàm render để in ra dữ liệu mới nhất
+	renderListProduct();
+}
+
+// -Giai đoạn 1: Là khi bấm vào button cập nhật => in thông tin của sản phẩm lên các input
+// Bước 1: Gán function cho button Cập nhật
+function getProductUpdate(productId) {
+	console.log('productId: ', productId);
+	let productFind = null;
+	// Bước 2: Khi function được gọi =>
+	// xử lý để lấy được thông tin sản phẩm mà người dùng click
+	for (let product of listProduct) {
+		if (product.id === productId) {
+			productFind = product;
+		}
+	}
+	// Bước 3: Đổ dữ liệu lên các input tương ứng
+	// Mở modal lên
+	myModal.show();
+	$idProduct.value = productFind.id;
+	$idProduct.disabled = true;
+	$titleProduct.value = productFind.title;
+	$priceProduct.value = productFind.price;
+	$imageProduct.value = productFind.image;
+	$descriptionProduct.value = productFind.description;
+	// Bước 4: Show ra button "Cập nhật" và ẩn button "Thêm mới"
+	$btnUpdateProduct.style.display = 'inline';
+	$btnCreateProduct.style.display = 'none';
+}
+
+// -Giai đoạn 2:
+// Bước 1: Gán function cho button Cập nhật trong modal
+$btnUpdateProduct.addEventListener('click', function () {
+	// Bước 2: Lấy các thông tin mà người dùng vừa chỉnh sửa
+	const id = $idProduct.value;
+	const title = $titleProduct.value;
+	const price = $priceProduct.value;
+	const image = $imageProduct.value;
+	const description = $descriptionProduct.value;
+	// Bước 3: Tìm vị trí của sản phẩm trong danh sách (tìm index product trong listProduct)
+	let indexProduct = -1;
+	for (let i = 0; i < listProduct.length; i++) {
+		if (listProduct[i].id === Number(id)) {
+			indexProduct = i;
+		}
+	}
+	// Bước 4: Sửa thông tin sản phẩm
+	listProduct[indexProduct].title = title;
+	listProduct[indexProduct].price = price;
+	listProduct[indexProduct].image = image;
+	listProduct[indexProduct].description = description;
+	// Bước 5: Sau khi xử lý dữ liệu => lưu thông tin vào trong local storage
+	localStorage.setItem('listProduct', JSON.stringify(listProduct));
+	// Bước 6: Gọi lại hàm renderListProduct để in ra thông tin sản phẩm mới nhất
+	renderListProduct();
+	// Bước 7: Khôi phục lại trạng thái ban đầu của giao diện
+	// Ẩn button Cập nhật
+	$btnUpdateProduct.style.display = 'none';
+	// Show button Thêm mới
+	$btnCreateProduct.style.display = 'inline';
+	// Reset input
+	$idProduct.value = '';
+	$idProduct.disabled = false;
+	$titleProduct.value = '';
+	$priceProduct.value = '';
+	$imageProduct.value = '';
+	$descriptionProduct.value = '';
+	// Đóng modal
+	myModal.hide();
+});
